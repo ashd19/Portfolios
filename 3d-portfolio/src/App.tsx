7d010ee90b4886ca95c12db601279665d21c8b90
ashton <ashtondsouza192@gmail.com>
2025-08-28 19:06:46 +0530

    Testimonials added , improve the ui maybe , and more improvs , taking a break now  and then coming back after some time (days)

--- 3d-portfolio/src/App.tsx
+++ 3d-portfolio/src/App.tsx
@@ -1,18 +1,20 @@
 import Hero from "./sections/Hero";
- //src/assets/constants/avatar-group-demo.tsx
+//src/assets/constants/avatar-group-demo.tsx
 import Projects from "./sections/Projects";
 import NavBar from "./sections/NavBar";
 import IntroBar from "./sections/IntroBar";
+import Testimonials from "./sections/Testimonials";
 const App = () => {
   return (
     <>
       <NavBar />
-      <div className="mt-">
+      <div className="mt-40">
         <IntroBar />
       </div>
       <Hero />
       {/* project section */}
       <Projects />
+      <Testimonials />
     </>
   );
 };
