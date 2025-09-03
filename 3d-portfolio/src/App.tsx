511e31ba280978654a6a74d19ac9c42ba3f9f02f
ashton <ashtondsouza192@gmail.com>
2025-09-03 20:40:27 +0530

    earth 3d model fix kar

--- 3d-portfolio/src/App.tsx
+++ 3d-portfolio/src/App.tsx
@@ -6,25 +6,27 @@ import Testimonials from "./sections/Testimonials";
 import Resume from "./sections/Resume";
 import Contact from "./sections/ContactForm";
 import Experience from "./sections/Experience";
+// import EarthCanvas from "./components/3d-models/Earth";
 
 const App = () => {
   return (
     <>
-     
-        <NavBar />
-        <div className="mt-40"></div>
-        <Hero />
-        {/* project section */}
-        <div className="mt-50">
-          <Projects />
-        </div>
-        <Experience/>
-        <Testimonials />
-        <div className="">
-          <Resume />
-        </div>
-        <Contact />
-     
+      <NavBar />
+      <div className="mt-40"></div>
+      <Hero />
+      {/* project section */}
+      <div className="mt-50">
+        <Projects />
+      </div>
+      <Experience />
+      <Testimonials />
+      <div className="">
+        <Resume />
+      </div>
+      <Contact />
+      {/* <div>
+        <EarthCanvas />
+      </div> */}
     </>
   );
 };
